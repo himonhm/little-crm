487c6b8bf6177e95f48da521fee05b68d9ed9660
Ilya Manannikov <himonhm@yandex.ru>
2024-03-16 15:03:40 +0300

    change registration form and template

--- mysite/authapp/forms.py
+++ mysite/authapp/forms.py
@@ -3,9 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserCreationFormWithExtraFields(UserCreationForm):
-    email = forms.CharField(max_length=100, required=True)
+class CustomUserCreationForm(UserCreationForm):
+    widget_attrs = {"class": "form-control", "id": "floatingText"}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in ["username", "password1", "password2", "email"]:
+            self.fields[field_name].widget.attrs.update(self.widget_attrs)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("email", "username", "password1", "password2")
